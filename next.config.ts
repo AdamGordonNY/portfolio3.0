7408e80734cbbc2de93bc075bc6b17eaf07d816a
adam <adamg42685@gmail.com>
2024-10-28 04:26:46 -0400

    added eslint and prettier configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
 
-const nextConfig: NextConfig = {
-  /* config options here */
+        pathname: "/**",
+        hostname: "my.certifyme.online",
+      },
+      {
+        protocol: "https",
+        pathname: "/**",
+        hostname: "utfs.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
